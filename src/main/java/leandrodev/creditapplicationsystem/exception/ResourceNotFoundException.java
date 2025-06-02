877ad687d8f87bc0374bcab185a1fd217c83494d
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-06-02 13:21:29 -0300

    Exceptio

---
+++ src/main/java/leandrodev/creditapplicationsystem/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package leandrodev.creditapplicationsystem.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
