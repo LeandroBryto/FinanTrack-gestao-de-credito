dd095d07a6f9553cc31c4e3841bf8e28a6e155c4
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-06-01 21:31:55 -0300

    feat: criação da entidade Client , Credit , User com atributos e relacionamentos

---
+++ src/main/java/leandrodev/creditapplicationsystem/entity/Credit.java
@@ -0,0 +1,25 @@
+package leandrodev.creditapplicationsystem.entity;
+
+import leandrodev.creditapplicationsystem.entity.abstracts.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Table(name = "credits")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Credit extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private CreditApplication creditApplication;
+
+    private Double creditLimit;
+    
+}
